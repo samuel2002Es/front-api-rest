d4797deb8368ada8174af76852e64b5a7941ffb4
samuel <samuel2002espinoza@gmail.com>
2023-04-19 16:41:48 -0600

    termine de hacer el login y voy por la consulta con el backend

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: null,
   },
   getters: {
   },
   mutations: {
+    setToken(state, payload){
+      state.token = payload;
+    },
   },
   actions: {
+    async login({commit},usuario){
+      try {
+        const res = await fetch('https://jwt-backend-production.up.railway.app/api/user/login')
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <HelloWorld />
+  <v-sheet width="300" class="mx-auto">
+    <v-form validate-on="submit" @submit.prevent="login(usuario)">
+      <v-text-field
+        v-model="usuario.email"
+        label="Correo Electronico"
+      ></v-text-field>
+      <v-text-field
+        v-model="usuario.password"
+        label="COntraseña"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+/* siempre los map accion van en los metodos */
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import { mapActions} from 'vuex'
 
-export default defineComponent({
+export default {
   name: 'HomeView',
-
-  components: {
-    HelloWorld,
+  data(){
+    return{
+      usuario:{
+        email: '',
+        password: '',
+      }
+    }
   },
-});
+  components: {
+  },
+  methods: {
+    ...mapActions(['login'])
+  }
+};
 </script>
